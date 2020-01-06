249f5aa15a7f211097653e79127fbe87981da269
㊥Maruchu <maruchu_com@yahoo.co.jp>
2020-01-07 05:56:40 +0900

    最初の更新

---
+++ Assets/MaruchuTextureChecker/MaruchuTextureChecker.cs
@@ -0,0 +1,188 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEngine.Profiling;
+using System.Collections.Generic;
+using UnityEditor;
+
+
+
+/// <summary>
+/// 配下のオブジェクトが持っているテクスチャの一覧と容量を表示してくれるスクリプト
+/// </summary>
+public class MaruchuTextureChecker : MonoBehaviour
+{
+    //チェックするテクスチャのパラメータ
+    [SerializeField]
+    private string[] checkPropNameArray = new string[]
+    {
+        //基本的にはこれだけあればOK
+        "_MainTex",
+        //↓のものはお好みで追加・削除してください
+
+        //代表的なもの
+        "_SubTex",
+        "_BumpMap",
+        "_NormalMap",
+
+        //ユニティちゃんが持ってるパラメータ
+        "_FalloffSampler",
+        "_RimLightSampler",
+        "_SpecularReflectionSampler",
+        "_EnvMapSampler",
+        "_NormalMapSampler",
+    };
+
+    //初期化フラグ
+    [HideInInspector]
+    public bool initializedFlag = false;
+
+    //結果表示　使用メモリ用
+    [SerializeField, Multiline(2)]
+    private string resultMemory = string.Empty;
+    //結果表示　サイズ別の枚数表示用
+    [SerializeField, Multiline(8)]
+    private string resultSize = string.Empty;
+    //結果表示　一覧表示用
+    [SerializeField, Multiline(10)]
+    private string resultLog = string.Empty;
+
+    //マテリアル一覧表示用
+    [SerializeField]
+    private List<Material> resultMatList = new List<Material>();
+    //テクスチャ一覧表示用
+    [SerializeField]
+    private List<Texture> resultTexList = new List<Texture>();
+
+    //サイズの記憶用
+    private Dictionary<int, int> texSizeDict = new Dictionary<int, int>();
+
+
+
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    private void Awake()
+    {
+        //実行してたら起動時に一回書き出し
+        Check();
+    }
+    /// <summary>
+    /// テクスチャのチェックと書き出し実行
+    /// </summary>
+    public void Check()
+    {
+        //初期化
+        resultMatList.Clear();
+        resultTexList.Clear();
+        texSizeDict.Clear();
+
+        //容量チェック
+        Renderer[] array = gameObject.GetComponentsInChildren<Renderer>();
+        foreach (Renderer rend in array)
+        {
+            //Material確認
+            foreach (Material mat in rend.materials)
+            {
+                //Texture確認
+                foreach (string propName in checkPropNameArray)
+                {
+                    //プロパティの存在を確認
+                    if (!mat.HasProperty(propName))
+                    {
+                        continue;
+                    }
+                    //プロパティ名を確認
+                    Texture tempTex = mat.GetTexture(propName);
+                    if (null == tempTex)
+                    {
+                        continue;
+                    }
+
+                    //新しいテクスチャ？
+                    if (!resultTexList.Contains(tempTex))
+                    {
+                        //テクスチャを記憶
+                        resultTexList.Add(tempTex);
+
+                        //Dictionaryでサイズ別に枚数を記憶
+                        int sizeX = (int)(1.0f / tempTex.texelSize.x);
+                        if (texSizeDict.ContainsKey(sizeX))
+                        {
+                            //枚数を+1
+                            texSizeDict[sizeX]++;
+                        }
+                        else
+                        {
+                            //1枚め
+                            texSizeDict.Add(sizeX, 1);
+                        }
+                    }
+                }
+
+                //新しいMaterial？
+                if (!resultMatList.Contains(mat))
+                {
+                    //Materialを記憶
+                    resultMatList.Add(mat);
+                }
+            }
+        }
+
+        //書き出し
+        string texResult = name + " 以下の取得可能なテクスチャ一覧\n\n";
+        long allMem = 0;
+        foreach (var temp in resultTexList)
+        {
+            //容量取得
+            long useMem = Profiler.GetRuntimeMemorySizeLong(temp);
+            allMem += useMem;
+            //サイズ(Xで確認)
+            int sizeX = (int)(1.0f / temp.texelSize.x);
+            texResult += temp.name + " (" + sizeX + "px, " + (useMem / 1024f / 1024f).ToString("0.00") + "MB)\n";
+        }
+        string sizeResult = name + " 以下のテクスチャサイズ別 枚数\n\n";
+        foreach (var key in texSizeDict.Keys)
+        {
+            //枚数一覧
+            sizeResult += key + "px = " + texSizeDict[key] + "枚\n";
+        }
+
+        //Inspector表示用
+        resultMemory = (allMem / 1024f / 1024f).ToString("0.00") + " MByte\n(" + allMem + " Byte)";
+        resultSize = sizeResult;
+        resultLog = texResult;
+        //デバッグ出力
+        Debug.LogWarning("---- MARUCHU TEXTURE CHECKER ----\nMemSize = " + (allMem / 1024f / 1024f).ToString("0.00") + "MB\n\n--------\n" + sizeResult + "\n--------\n" + texResult + "\n--------\n");
+    }
+}
+
+/// <summary>
+/// Inspector表示用
+/// </summary>
+[CustomEditor(typeof(MaruchuTextureChecker))]
+public class MaruchuTextureCheckerEditor : Editor
+{
+    /// <summary>
+    /// Inspector表示用
+    /// </summary>
+    public override void OnInspectorGUI()
+    {
+        //再チェック用にボタンを付ける
+        {
+            //元のスクリプトを取得
+            MaruchuTextureChecker targetScript = target as MaruchuTextureChecker;
+            //チェックする？                                       あるいは最初の一回
+            if (GUILayout.Button("配下のテクスチャをチェック") || !targetScript.initializedFlag)
+            {
+                //ボタンが押された
+                targetScript.Check();
+                //チェック済み
+                targetScript.initializedFlag = true;
+            }
+        }
+
+        //親のGUI表示をそのままやる
+        base.OnInspectorGUI();
+    }
+}
+#endif
---
+++ README.md
@@ -0,0 +1,29 @@
+﻿# MaruchuTextureChecker
+㊥Maruchuテクスチャチェッカー<br>
+
+Copyright(C) 2020 [㊥Maruchu](https://twitter.com/Maruchu "㊥Maruchu")
+
+
+<br><br><br><br>
+
+## 使い方
+
+キャラや背景の3Dモデルを使った作業中に、現在 使用しているテクスチャの一覧が見られると便利ですよね。<br>
+というわけで貼り付けると配下のオブジェクトが持っているテクスチャを探してきて一覧表示してくれるスクリプトです。
+
+プロジェクトに MaruchuTextureChecker というスクリプトが入っているので、<br>
+Renderer を持っている適当なオブジェクトの Inspector から AddComponent してみてください。<br>
+(適当な3Dモデルデータが無い場合、AssetStore でユニティちゃんモデルなどをDLしてお使いください)
+
+
+<br><br><br><br>
+
+## 結果の見方
+
+スクリプトを AddComponent すると、結果が Console と Inspector に表示されます
+
+
+<br><br><br><br>
+
+## ライセンス
+㊥MaruchuテクスチャチェッカーにはMITライセンスが適用されています<br>
